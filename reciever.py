cdaded038d6dbb1178d245aafd820c1585264f81
Ashish <ashishkumaraswamy@gmail.com>
2021-11-16 21:58:07 +0530

    first commit added basic functionalities

---
+++ reciever.py
@@ -0,0 +1,146 @@
+#Importing the libraries
+import socket
+import os
+from os import system
+import time
+#----------------------------------------------------------
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 4456
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+CLIENT_PATH="client_data"
+
+def title_page(): #TITLE PAGE UI
+    system("cls")
+    print("\n\t\t*******FILE SHARING APPICATION********")
+    print("\n\n\tFile Sharing application that lets you upload your files onto a server from your local storage,\n and also allows you to download others file from the server.\n\n")
+
+def new_user_ques():  #NEW USER INPUT
+    print("\n\tAre you a new user or an existing user?(y/n): ")
+    msg=input()
+    return msg
+
+def retry_password(client):  #RETRY PASSWORD FOR AUTHENTICATION
+    while True:
+        msg=client.recv(SIZE).decode(FORMAT)
+        passw=input("\n\t"+msg)
+        client.send(passw.encode(FORMAT))
+        msg=client.recv(SIZE).decode(FORMAT)
+        if "Access" in msg:
+            break
+        else:
+            print("\n\t Invalid Password Try Again") 
+            system("cls")
+            title_page()
+    
+def authentication(client):  #AUTHENTICATION OF USER
+    new_user_check=client.recv(SIZE).decode(FORMAT)
+    checkv=input(new_user_check)
+    client.send(checkv.encode(FORMAT))
+    if checkv.lower()=='y':
+        title_page()
+        msg=client.recv(SIZE).decode(FORMAT)
+        username=input('\t'+msg)
+        client.send(username.encode(FORMAT))
+        msg=client.recv(SIZE).decode(FORMAT)
+        password=input('\t'+msg)
+        client.send(password.encode(FORMAT))
+        msg=client.recv(SIZE).decode(FORMAT)
+        title_page()
+        print("\n\n\t",msg)
+        time.sleep(3)
+        return
+    else:
+        title_page()
+        msg=client.recv(SIZE).decode(FORMAT)
+        username=input('\t'+msg)
+        client.send(username.encode(FORMAT))
+        msg=client.recv(SIZE).decode(FORMAT)
+        password=input('\t'+msg)
+        client.send(password.encode(FORMAT))
+        msg=client.recv(SIZE).decode(FORMAT)
+        if "Access" in msg:
+            title_page()
+            print("\n\n\t",msg)
+            time.sleep(3)
+        else:
+            print("\n\n\t"+msg)
+            time.sleep(3)
+            title_page()   
+            retry_password(client)
+        return
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    authentication(client)
+    title_page()
+    while True:
+        data = client.recv(SIZE).decode(FORMAT)
+        print(data)
+        cmd, msg = data.split("@")
+
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            if msg!="":
+                print(f"{msg}")
+
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "HELP":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        elif cmd == "LIST":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "DELETE":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        elif cmd == "UPLOAD":
+            path = data[1]
+
+            with open(f"{path}", "r") as f:
+                text = f.read()
+
+            filename = path.split("/")[-1]
+            send_data = f"{cmd}@{filename}@{text}"
+            client.send(send_data.encode(FORMAT))
+        elif cmd == "DOWNLOAD":
+            client.send(cmd.encode(FORMAT))
+            listdir=client.recv(SIZE).decode(FORMAT)
+            system("cls")
+            title_page()
+            print("\n\t List Of Directories Availbale:")
+            print("\n\t  ",listdir)
+            user_dir=input("\n\tEnter the directory you want to download from:")
+            client.send(user_dir.encode(FORMAT))
+            listfiles=client.recv(SIZE).decode(FORMAT)
+            if "empty" in listfiles:
+                print("\n\t",listfiles)
+                time.sleep(3)
+                continue
+            else: 
+                print("\n\t",listfiles)
+                user_file=input("Enter the file you wish to download: ")
+                client.send(user_file.encode(FORMAT))
+                file=client.recv(SIZE).decode(FORMAT)
+                if "Not Exist" in file:
+                    print("\n\t",file)
+                else:
+                    filepath = os.path.join(CLIENT_PATH, user_file)
+                    with open(filepath, "w") as f:
+                        f.write(file)
+                    print("File Downloaded Successfully")
+
+    print("Disconnected from the server.")
+    client.close()
+
+if __name__ == "__main__":
+    title_page()
+    main()
---
+++ server.py
@@ -0,0 +1,165 @@
+#Importing the libraries
+import os
+import socket
+import threading
+import time
+#----------------------------------------------------------
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 4456
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+def listdir(path=""): #FUNCTION TO LIST THE DIRECTORIES IN THE SERVER DIRECTORY
+    send_data="OK@"
+    files = os.listdir(SERVER_DATA_PATH+"\\"+path)
+    if len(files) == 0:
+        send_data += "The server directory is empty"
+    else:
+        send_data += "\n".join(f for f in files)
+    return send_data
+
+def handle_client(conn, addr):
+    username=authentication_of_user(conn)
+    print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send("OK@Welcome to the File Server.".encode(FORMAT))
+
+    while True:
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+
+        if cmd == "LIST":
+            send_data=listdir()
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "UPLOAD":
+            name, text = data[1], data[2]
+            filepath = os.path.join(SERVER_DATA_PATH,username, name)
+            try:
+                os.listdir(SERVER_DATA_PATH+"\\"+username)
+            except:
+                os.mkdir(SERVER_DATA_PATH+"\\"+username)
+            with open(filepath, "w") as f:
+                f.write(text)
+            send_data = "OK@File uploaded successfully."
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "DELETE":
+            files = os.listdir(SERVER_DATA_PATH+"\\"+username)
+            send_data = "OK@"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    cmd="del \""+SERVER_DATA_PATH+"\\"+username+"\\"+filename+"\""
+                    os.system(cmd)
+                    send_data += "File deleted successfully."
+                else:
+                    send_data += "File not found."
+
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            break
+
+        elif cmd == "DOWNLOAD":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data=""
+            print(files)
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                send_data += "\n".join(f for f in files)
+            conn.send(send_data.encode(FORMAT))
+            user_dir=conn.recv(SIZE).decode(FORMAT)
+            items_in_dir=listdir(user_dir)
+            if "empty" in items_in_dir:
+                conn.send(items_in_dir.encode(FORMAT))
+            else:
+                conn.send(items_in_dir.encode(FORMAT))
+                user_file=conn.recv(SIZE).decode(FORMAT)
+                path=SERVER_DATA_PATH+'\\'+user_dir+'\\'+user_file
+                try:
+                    with open(path, "r") as f:
+                        text = f.read()
+                    filename = path.split("/")[-1]
+                    send_data = f"{filename}"
+                    conn.send(send_data.encode(FORMAT))
+                    conn.send("OK@ ".encode(FORMAT))
+                except:
+                    conn.send("File Does Not Exist under this name".encode(FORMAT))
+                    conn.send("OK@ ".encode(FORMAT))
+
+        elif cmd == "HELP":
+            data = "OK@"
+            data += "LIST: List all the files from the server.\n"
+            data += "UPLOAD <path>: Upload a file to the server.\n"
+            data += "DELETE <filename>: Delete a file from the server.\n"
+            data += "LOGOUT: Disconnect from the server.\n"
+            data += "HELP: List all the commands."
+
+            conn.send(data.encode(FORMAT))
+
+    print(f"[DISCONNECTED] {addr} disconnected")
+    conn.close()
+
+def retry_password(conn,username,password):
+    while True:
+        conn.send("Re Enter Password: ".encode(FORMAT))
+        passw=conn.recv(SIZE).decode(FORMAT)
+        if password==passw:
+            return 
+        else:
+            conn.send("Invalid Password: ".encode(FORMAT))
+
+def authentication_of_user(conn):
+    conn.send("Are you a new user or an existing user?(y/n): ".encode(FORMAT))
+    new_user_check=conn.recv(SIZE).decode(FORMAT)
+    username=""
+    if new_user_check=='y':
+        conn.send("Username:".encode(FORMAT))
+        username=conn.recv(1024).decode(FORMAT)
+        conn.send("Password:".encode(FORMAT))
+        password=conn.recv(1024).decode(FORMAT)
+        f=open('password_file\password_file.txt','w+')
+        text=username+" "+password
+        f.write(text)
+        f.close()
+        conn.send("From Server: Access Granted .....LOADING.....".encode(FORMAT))
+    else:
+        conn.send("Username:".encode(FORMAT))
+        username=conn.recv(1024).decode(FORMAT)
+        conn.send("Password:".encode(FORMAT))
+        password=conn.recv(1024).decode(FORMAT)
+        with open('password_file\password_file.txt','r') as f:
+            lines=f.readline()
+            usernamef=lines.split()[0]
+            passwordf=lines.split()[1]
+            if username==usernamef:
+                if password==passwordf:
+                    conn.send("From Server: Access Granted .....LOADING.....".encode(FORMAT))
+                else:
+                    conn.send("Invalid Password".encode(FORMAT))
+                    retry_password(conn,username,passwordf)
+                    conn.send("From Server: Access Granted .....LOADING.....".encode(FORMAT))
+    return username
+
+def main():
+    print("[STARTING] Server is starting")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+if __name__ == "__main__":
+    main()
